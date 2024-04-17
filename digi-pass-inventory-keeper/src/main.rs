1746c0580ae3e14d94497c81574180fb2d439fcb
Vicente Albonian <vicente.albonian@ticketek.com.au>
2024-04-17 23:04:02 +0800

    added cf stack for inventory keeper

--- digi-pass-inventory-keeper/src/main.rs
+++ digi-pass-inventory-keeper/src/main.rs
@@ -5,6 +5,7 @@ mod constants;
 
 use std::env;
 
+use aws_sdk_secretsmanager::types::Filter;
 use dotenvy::dotenv;
 use mongodb::Client;
 use tracing_subscriber::fmt;
@@ -13,10 +14,14 @@ use crate::{application::InventoryKeeperService, persistence::{InventoryReposito
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()>{
-
-    println!("loading env variables from file");
-    if dotenv().is_ok() {
-        println!("Env variables from file successful");
+    
+    if is_running_in_lambda() {
+        load_secrets().await.expect("failed to load secrets");
+    }else{
+        println!("loading env variables from file");
+        if dotenv().is_ok() {
+            println!("Env variables from file successful");
+        }
     }
     let connection_string = env::var("MongoDbConfig__ConnectionString")
         .expect("MongoDb connection string not found.");
@@ -39,5 +44,35 @@ async fn main() -> anyhow::Result<()>{
     //we probably want to add a retry mechanism here
     keeper_service.do_inventory_keeping().await.unwrap();
 
+    Ok(())
+}
+
+
+fn is_running_in_lambda() -> bool {
+    env::var("AWS_LAMBDA_FUNCTION_NAME").is_ok()
+}
+
+pub async fn load_secrets() -> anyhow::Result<()> {
+    
+    if !is_running_in_lambda() {
+        tracing::info!("Not in lambda, not loading secrets");
+        return Ok(())
+    }
+    tracing::debug!("Loading Secrets:");
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_secretsmanager::Client::new(&config);
+    let filter = Filter::builder().key("name".into()).values("DigiPass__").build();
+    let resp = client.list_secrets().filters(filter).send().await?;
+    let secrets = resp.secret_list();
+    tracing::debug!("Loading Secrets 222: {:?}", secrets);
+    for secret in secrets {
+        
+        tracing::debug!("Secret found: {:?}", secret.name());
+        if let Some(name) = secret.name() {
+            let resp = client.get_secret_value().secret_id(name).send().await?;
+            let name = name.replace("DigiPass__", "");
+            env::set_var(&name, resp.secret_string().ok_or(anyhow::anyhow!("Failed getting secret"))?);
+        }
+    }
     Ok(())
 }
